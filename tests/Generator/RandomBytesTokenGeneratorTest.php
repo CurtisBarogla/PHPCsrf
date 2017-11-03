83463235e3a78fcd605baff6dbae2d5180507f11
CurtisBarogla <curtis_barogla@outlook.fr>
2017-11-03 06:21:28 +0100

    Change method testing to not doing useless assertions

--- tests/Generator/RandomBytesTokenGeneratorTest.php
+++ tests/Generator/RandomBytesTokenGeneratorTest.php
@@ -15,6 +15,7 @@ namespace ZoeTest\Component\Csrf\Generator;
 use PHPUnit\Framework\TestCase;
 use Zoe\Component\Csrf\Generator\RandomBytesTokenGenerator;
 use Zoe\Component\Csrf\Exception\InvalidArgumentException;
+use Zoe\Component\Csrf\Generator\TokenGeneratorInterface;
 
 /**
  * RandomBytesTokenGenerator testcase 
@@ -28,6 +29,16 @@ class RandomBytesTokenGeneratorTest extends TestCase
 {
     
     /**
+     * @see \Zoe\Component\Csrf\Generator\RandomBytesTokenGenerator
+     */
+    public function testInterface(): void
+    {
+        $generator = new RandomBytesTokenGenerator(7);
+        
+        $this->assertInstanceOf(TokenGeneratorInterface::class, $generator);
+    }
+    
+    /**
      * @see \Zoe\Component\Csrf\Generator\RandomBytesTokenGenerator::generate()
      */
     public function testGenerate(): void
@@ -36,13 +47,21 @@ class RandomBytesTokenGeneratorTest extends TestCase
         
         // not really pertinent... but i like it ><
         $stored = null;
-        for ($i = 0; $i < 100; $i++) {
+        $same = false;
+        $regexOk = true;
+        for ($i = 0; $i < 1000; $i++) {
             $stored = $generator->generate();
-            $this->assertNotSame($stored, $generator->generate());
-            $this->assertRegExp("#[A-Za-z0-9]#", $generator->generate());
+            $same = $stored === $generator->generate();
+            if($same)
+                break;
+            $regexOk = 1 === \preg_match("#[A-Za-z0-9]#", $generator->generate());
+            if(!$regexOk)
+                break;
             $stored = null;
         }
         
+        $this->assertFalse($same);
+        $this->assertTrue($regexOk);
     }
     
                     /**_____EXCEPTIONS_____**/
