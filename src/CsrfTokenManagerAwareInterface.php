3a0c97134cc5e8d2909252fcbf04b68b4b0dda96
CurtisBarogla <curtis_barogla@outlook.fr>
2019-01-08 20:04:23 +0100

    Add an accessor for the manager

--- src/CsrfTokenManagerAwareInterface.php
+++ src/CsrfTokenManagerAwareInterface.php
@@ -22,6 +22,14 @@ interface CsrfTokenManagerAwareInterface
 {
     
     /**
+     * Get the linked csrf token manager
+     * 
+     * @return CsrfTokenManagerInterface
+     *   Linked csrf token manager
+     */
+    public function getManager(): CsrfTokenManagerInterface;
+    
+    /**
      * Link a csrf token manager to the component
      * 
      * @param CsrfTokenManagerInterface $manager
